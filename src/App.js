e6990995676a415ea116ab8f553dd13dd75f0751
razack00 <121952296+razack00@users.noreply.github.com>
2024-03-26 20:46:46 +0100

    added routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import "./index.css"
+import Dashboard from "./pages/Dashboard"
+import Home from "./pages/Home"
+import NavBar from "./components/NavBar"
+import About from "./pages/About"
 
 function App() {
+
   return (
-    <>
-      
-    </>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={ <Home /> } exact />
+        <Route path="/about" element={ <About /> } />
+        <Route path="/dashboard" element={ <Dashboard /> } />
+      </Routes>
+    </Router>
   )
 }
-
 export default App
