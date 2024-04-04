43186a5406dd97bfc70a26279d9dac9f39d258fd
razack00 <121952296+razack00@users.noreply.github.com>
2024-04-04 16:37:11 +0100

    About init

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import Container from "react-bootstrap/Container"
+function About() {
+
+  return (
+    <Container className="my-4">
+        <h1>About Budgetr</h1>
+    </Container>
+  )
+}
+
+export default About
